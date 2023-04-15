93b8c3221a74eaa26dcb5a8d6a11ae5baf9bc2da
cristian.bahamode <cristian.bahamonde.santamaria@gmail.com>
2023-04-15 21:42:50 +0200

    fix

--- Index.js
+++ Index.js
@@ -1,43 +1,39 @@
-class Persona
-{
-    constructor(nombre, fechaNacimiento, fechaDefuncion, wiki, img)
-    {
-       this.nombre=nombre;
-       this.fechaNacimiento=fechaNacimiento;
-       this.fechaDefuncion=fechaDefuncion;
-       this.wiki=wiki;
-       this.img=img;
+class Persona {
+    constructor(nombre, fechaNacimiento, fechaDefuncion, wiki, img) {
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.fechaDefuncion = fechaDefuncion;
+        this.wiki = wiki;
+        this.img = img;
     }
 }
 
 
-function guardarPersona(event)
-{
-    var name = event.target.textContent;
+function guardarPersona(event) {
     const personas = JSON.parse(localStorage.getItem("Personas"));
-    for(var i=0; i<personas.length; i++);
-    {
-        if(personas[i].nombre===name)
-        {
-            var personaJSON= JSON.stringify(personas[i]);
-            localStorage.setItem("persona", personaJSON);
+    const nombre = event.target.innerHTML;
+    for (var i = 0; i < personas.length; i++) {
+        if (personas[i].nombre === nombre) {
+            var personaJson = JSON.stringify(personas[i]);
+            localStorage.setItem("Persona", personaJson);
+            return;
         }
     }
+
 }
 
 
-function mostrarPersonas()
-{
+function mostrarPersonas() {
     const personas = JSON.parse(localStorage.getItem("Personas"));
     const fila = document.getElementById("filasPersona");
     //const img =
-    for(var i=0; i<personas.length; i++){
+    for (var i = 0; i < personas.length; i++) {
         const td = document.createElement("td");
         const a = document.createElement("a");
-        a.addEventListener("click", guardarPersona(event));
-        a.href="Persona.html";
-        a.innerHTML=personas[i].nombre;
-     // td.appendChild(img);
+        a.onclick = guardarPersona;
+        a.href = "Persona.html";
+        a.innerHTML = personas[i].nombre;
+        // td.appendChild(img);
         td.appendChild(a);
         fila.appendChild(td);
     }
@@ -45,34 +41,30 @@ function mostrarPersonas()
 
 
 
-function cargarDatos()
-{
-    var usersArray=[{user: "x", password:"x"}, {user: "y", password:"y"}, {user: "z", password:"z"}];
-    var users= JSON.stringify(usersArray);
+function cargarDatos() {
+    var usersArray = [{ user: "x", password: "x" }, { user: "y", password: "y" }, { user: "z", password: "z" }];
+    var users = JSON.stringify(usersArray);
     localStorage.setItem("Usuarios", users);
-    var personas= [];
-    var persona= new Persona("Ricardo", "2001", "hoy", "https://es.wikipedia.org/wiki/HTML", "");
+    var personas = [];
+    var persona = new Persona("Ricardo", "2001", "hoy", "https://es.wikipedia.org/wiki/HTML", "");
     personas.push(persona);
-    var personasJSON= JSON.stringify(personas);
+    var personasJSON = JSON.stringify(personas);
     localStorage.setItem("Personas", personasJSON);
     mostrarPersonas();
 }
 
 
-function logearse()
-{
-    const fieldUser= document.getElementById("campoUsuario").value;
-    const fieldPassword= document.getElementById("campoContrasenia").value;
+function logearse() {
+    const fieldUser = document.getElementById("campoUsuario").value;
+    const fieldPassword = document.getElementById("campoContrasenia").value;
     const usuariosBD = JSON.parse(localStorage.getItem("Usuarios"));
-    for(var i=0; i<usuariosBD.length; i++)
-    {
-        if(usuariosBD[i].user===fieldUser && usuariosBD[i].password===fieldPassword)
-        {   
+    for (var i = 0; i < usuariosBD.length; i++) {
+        if (usuariosBD[i].user === fieldUser && usuariosBD[i].password === fieldPassword) {
             window.alert("Usuario válido, bienvenido!");
-            window.location.href='Login.html';
-            return ;
+            window.location.href = 'Login.html';
+            return;
         }
-    }  
+    }
     window.alert("Usuario no válido, intentelo de nuevo");
 }
 
