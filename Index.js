5bf402ba3752f04e01617af80eae11396c58a572
cristian.bahamode <cristian.bahamonde.santamaria@gmail.com>
2023-04-16 21:49:20 +0200

    fix createElements

--- Index.js
+++ Index.js
@@ -9,9 +9,8 @@ class Persona {
 }
 
 
-class Producto{
-    constructor(nombre, fechaCreacion, fechaFinalizacion, wiki, img, personas, entidades)
-    {
+class Producto {
+    constructor(nombre, fechaCreacion, fechaFinalizacion, wiki, img, personas, entidades) {
         this.nombre = nombre;
         this.fechaCreacion = fechaCreacion;
         this.fechaFinalizacion = fechaFinalizacion;
@@ -55,35 +54,30 @@ function mostrarPersonas() {
 
 
 
-function guardarProducto(event)
-{
-    const productos= JSON.parse(localStorage.getItem("Productos"));
+function guardarProducto(event) {
+    const productos = JSON.parse(localStorage.getItem("Productos"));
     const nombre = event.target.innerHTML;
-    for(var i=0; i<productos.length; i++)
-    {
-        if(productos[i].nombre===nombre)
-        {
+    for (var i = 0; i < productos.length; i++) {
+        if (productos[i].nombre === nombre) {
             var productoJSON = JSON.stringify(productos[i]);
             localStorage.setItem("Producto", productoJSON);
-            return ;
+            return;
         }
     }
 }
 
 
 
-function mostrarProductos()
-{
+function mostrarProductos() {
     const productos = JSON.parse(localStorage.getItem("Productos"));
     const fila = document.getElementById("filasProducto");
-    for (var i=0; i<productos.length; i++)
-    {
-        const td= document.getElementById("td");
-        const a = document.getElementById("a");
-        const img = document.getElementById("img");
-        img.src= productos[i].img;
+    for (var i = 0; i < productos.length; i++) {
+        const td = document.createElement("td");
+        const a = document.createElement("a");
+        const img = document.createElement("img");
+        img.src = productos[i].img;
         a.onclick = guardarProducto;
-        a.href="Producto.html";
+        a.href = "Producto.html";
         a.innerHTML = productos[i].nombre;
         td.appendChild(img);
         td.appendChild(a);
@@ -103,9 +97,9 @@ function cargarDatos() {
     localStorage.setItem("Personas", personasJSON);
     mostrarPersonas();
     var productos = [];
-    var producto = new Producto("Champú", "2003", "ayer", "https://es.wikipedia.org/wiki/HTML", "HTML.jpg", "si", "no");
+    var producto = new Producto("Champú", "2003", "ayer", "https://es.wikipedia.org/wiki/HTML", "HTML.png", "si", "no");
     productos.push(producto);
-    var productosJSON= JSON.stringify(productos);
+    var productosJSON = JSON.stringify(productos);
     localStorage.setItem("Productos", productosJSON);
     mostrarProductos();
 }
